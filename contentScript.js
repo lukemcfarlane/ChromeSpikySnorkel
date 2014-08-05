8242c2101f81206f9092664464a5e62dbeaa9e3b
Luke McFarlane <luke.mcfarlane@trineo.co.nz>
2014-08-05 16:23:02 +1200

    Show all link for related lists

--- contentScript.js
+++ contentScript.js
@@ -14,3 +14,17 @@ if(showMoreEl.size() === 1) {
     $('.fewerMore')
     	.html(showAllLink);
 }
+
+
+$('div.pShowMore a:first-child').each(function() {
+    var oldHrefDecoded = decodeURIComponent($(this).attr('href'));
+    var matchArr = oldHrefDecoded.match(/(javascript:showMoreList\('\/.{15}', ')(.*)(',.*)/);
+    var oldURL = matchArr[2];
+    var newURL = oldURL.replace(/rowsperlist=\d+/, 'rowsperlist=1000'); 
+    var newURLEncoded = encodeURIComponent(newURL);
+    var newHref = matchArr[1] + newURLEncoded + matchArr[3];
+    $(this).attr('href', newHref)
+        .text('Show all');
+});
+
+
