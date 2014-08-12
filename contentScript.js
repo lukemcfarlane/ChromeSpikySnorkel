c169c9f987abc85c1dca80afc1280bdfb78ac32d
Luke McFarlane <luke.mcfarlane@trineo.co.nz>
2014-08-12 14:59:05 +1200

    Added nav link tweaks, put each tweak in try/catch

--- contentScript.js
+++ contentScript.js
@@ -1,30 +1,45 @@
 console.log('Spiky Snorkel is now running...');
 
-var showMoreEl = $('img[alt="Show More"]');
+try {
+	var showMoreEl = $('img[alt="Show More"]');
 
-if(showMoreEl.size() === 1) {
-    var showAllURL = showMoreEl
-    	.parent()
-    	.attr('href') + '9999999';
+	if (showMoreEl.size() === 1) {
+		var showAllURL = showMoreEl
+			.parent()
+			.attr('href') + '9999999';
 
-    var showAllLink = $('<a></a>');
+		var showAllLink = $('<a></a>');
 
-    showAllLink.attr('href', showAllURL);
-    showAllLink.text('Show all records');
-    $('.fewerMore')
-    	.html(showAllLink);
+		showAllLink.attr('href', showAllURL);
+		showAllLink.text('Show all records');
+		$('.fewerMore')
+			.html(showAllLink);
+	}
+} catch (err) {
+	console.log('Spiky Snorkel: \'show all\' tweak failed', err);
 }
 
 
-$('div.pShowMore a:first-child').each(function() {
-    var oldHrefDecoded = decodeURIComponent($(this).attr('href'));
-    var matchArr = oldHrefDecoded.match(/(javascript:showMoreList\('\/.*', ')(.*)(',.*)/);
-    var oldURL = matchArr[2];
-    var newURL = oldURL.replace(/rowsperlist=\d+/, 'rowsperlist=1000'); 
-    var newURLEncoded = encodeURIComponent(newURL);
-    var newHref = matchArr[1] + newURLEncoded + matchArr[3];
-    $(this).attr('href', newHref)
-        .text('Show all');
-});
+try {
+	$('div.pShowMore a:first-child').each(function() {
+		var oldHrefDecoded = decodeURIComponent($(this).attr('href'));
+		var matchArr = oldHrefDecoded.match(/(javascript:showMoreList\('\/.*', ')(.*)(',.*)/);
+		var oldURL = matchArr[2];
+		var newURL = oldURL.replace(/rowsperlist=\d+/, 'rowsperlist=1000');
+		var newURLEncoded = encodeURIComponent(newURL);
+		var newHref = matchArr[1] + newURLEncoded + matchArr[3];
+		$(this).attr('href', newHref)
+			.text('Show all');
+	});
+} catch (err) {
+	console.log('Spiky Snorkel: \'show all related list\' tweak failed', err);
+}
 
 
+try {
+	$('.navLinks .linkElements :contains(Help & Training)').hide()
+	var debugLogLink = $('a.debugLogLink');
+	$('.navLinks .linkElements').append(debugLogLink);
+} catch (err) {
+	console.log('Spiky Snorkel: \'nav links\' tweak failed', err);
+}
